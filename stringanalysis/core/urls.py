3bb53d95e2a04b7ec45eaadc7d4c217490f50d67
Nikhil Singh <nikhilsinghcr007@gmail.com>
2023-12-31 08:46:36 +0530

    Added Upper & Lower Case functionality

--- stringanalysis/core/urls.py
+++ stringanalysis/core/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Home, name='home'),
-    path('<int:count>/', views.Home, name='home'),
-    path('<str:reverse>/', views.Home, name='home'),
-    path('<int:words>', views.Home, name='home'),
+    path('', views.Home, name='home'), # The Home Page that handles both GET & POST requests!.
 ]
--- stringanalysis/core/views.py
+++ stringanalysis/core/views.py
@@ -1,51 +1,43 @@
 from django.shortcuts import render
 
-# def Home(request):
-#     if request.method == "POST":
-#         try:
-#             print('1')
-#             text = request.POST.get('textarea')
-#             print(text)
-
-#             cnt = request.POST.get('cnt')
-#             rev = request.POST.get('rev')
-#             wrd = request.POST.get('wrd')
-
-#             if cnt == 'count':
-#                 total_char = sum(1 for char in text if char != ' ')
-#                 return render(request, 'core/index.html', {'count': total_char})
-
-#             elif rev == 'reverse':
-#                 reversed_text = text[::-1]
-#                 return render(request, 'core/index.html', {'reverse': reversed_text})
-            
-#             elif wrd == 'words':
-#                 word_list = text.split()
-#                 return render(request, 'core/index.html', {'words': len(word_list), 'words_count': word_list})
-
-#         except Exception as e:
-#             print(e.__str__())
-
-#     return render(request, 'core/index.html')
-
 def Home(request):
+    '''
+        The Home function is the only and main functin that handles all the requests.
+        If request is POST then the action variable is checked for certain values, values are the type of action to be prtformed ont the text.
+        For instance if the value of the action is count, the if condition of with count will be executed, result will be again populated to the index.html.
+        The DTL in index.html is then responsible to dynamically display the content.
+    '''
+
     if request.method == "POST":
         try:
             text = request.POST.get('textarea')
 
             action = request.POST.get('action')
 
+            # Condition for Count value of action
             if action == 'count':
                 total_char = sum(1 for char in text if char != ' ')
                 return render(request, 'core/index.html', {'count': total_char})
 
+            # Condition for Reverse value of action
             elif action == 'reverse':
                 reversed_text = text[::-1]
                 return render(request, 'core/index.html', {'reverse': reversed_text})
             
+            # Condition for Count Words value of action
             elif action == 'words':
                 word_list = text.split()
                 return render(request, 'core/index.html', {'words': len(word_list), 'words_count': word_list})
+            
+            # Condition for Upper Case value of action
+            elif action == 'upper':
+                upper_text = text.upper()
+                return render(request, 'core/index.html', {'upper': upper_text})
+            
+            # Condition for Lower Case value of action
+            elif action == 'lower':
+                lower_text = text.lower()
+                return render(request, 'core/index.html', {'lower': lower_text})
 
         except Exception as e:
             print(e.__str__())
